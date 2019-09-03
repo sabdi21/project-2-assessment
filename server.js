609ce2a10885c2798699d8da1d034827d0a5a197
sabdi21 <sabdi21@hotmail.com>
2019-09-03 15:46:23 -0700

    test complete

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const methodOverride = require('method-override');
+const db = require('./models');
 
 const app = express();
 
@@ -9,14 +10,42 @@ app.use(express.static('static'));
 app.use(methodOverride('_method'));
 
 // WRITE YOUR ROUTES HERE /////////////////////
+
 app.get('/', (req, res) => {
-    res.render('index')
+    db.widget.findAll()
+    
+    .then((allWidgets) => {
+        res.render('index', {
+            allWidgets
+        });
+    });
 })
 app.post('/', (req, res) => {
-    console.log('STUB -- THE POST BUTTON WAS CLICKED')
+    console.log('results: ', req.body)
+    let newWidget = {
+        description: req.body.description,
+        quantity: req.body.quantity
+    }
+    db.widget.create(newWidget)
+    .then((widget) => {
+        res.redirect('/');
+    });
+   
+    // res.redirect('index');
 })
 
-app.get
+app.delete('/:idx', (req, res) => {
+    console.log('IS THIS ROUTE EVEN WORKING!!!! ')
+    console.log('this was deleted: ', req.params)
+    db.widget.destroy({
+        where: {
+            id: req.params.idx
+        }
+    }).then((widget) => {
+        res.redirect('/');
+    });
+
+})
 // YOUR ROUTES ABOVE THIS COMMENT /////////////
 
 app.listen(3000);
